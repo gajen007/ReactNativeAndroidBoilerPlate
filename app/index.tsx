1169171ff1f111f25b23db31097211b89c62bcec
Gajen Dissanayake <gajen007@gmail.com>
2026-03-03 11:51:22 -0500

    LoginScreen has done

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { router } from "expo-router";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    marginLeft:10
+  },
+  linkButton:{
+    borderWidth:1,
+    borderRadius:5,
+    borderColor:'#000000',
+    margin:10,
+    padding:5,
+  }
+});
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+        <Pressable style={styles.linkButton}onPress={()=>{router.push("/pages/LoginScreen")}}><Text>Login Screen</Text></Pressable>
+        <Pressable style={styles.linkButton}onPress={()=>{router.push("/pages/Buttons")}}><Text>Buttons Page</Text></Pressable>
+        <Pressable style={styles.linkButton}onPress={()=>{router.push("/pages/Timeline/Timeline")}}><Text>Timeline Example</Text></Pressable>
+    </View>
+  );
+}
